06166bf06b0d0657f688a889e12afd1253008f58
John Bennett <john.bennett@spr.com>
2023-12-22 16:12:49 -0600

    changes

--- skeema/schemas/AdventureWorks/TestTable.sql
+++ skeema/schemas/AdventureWorks/TestTable.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `TestTable` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `id` bigint NOT NULL,
   `FirstName` varchar(50) NOT NULL,
   `LastName` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+)
