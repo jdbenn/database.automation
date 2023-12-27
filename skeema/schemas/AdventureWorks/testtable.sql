d706344204bdf0c862210fff5f66f9e4f6cab408
John Bennett <john.d.bennett@hotmail.com>
2023-12-27 03:52:01 -0600

    added test table

---
+++ skeema/schemas/AdventureWorks/testtable.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `TestTable` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `FirstName` varchar(50) NOT NULL,
+  `LastName` varchar(100) NOT NULL,
+  PRIMARY KEY(id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Customers (resellers) of Adventure Works products.';
